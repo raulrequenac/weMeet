cb79a74da6fb51d1c8e5c5aae8bb14cdde98dbb4
raulrequenac <raulrequec@hotmail.com>
2019-12-05 21:34:06 +0100

    companies login

--- config/routes.js
+++ config/routes.js
@@ -1,19 +1,27 @@
 const express = require('express');
 const uploadCloud = require('../config/cloudinary.config.js');
 const usersController = require('../controllers/users.controller');
+const companiesController = require('../controllers/companies.controller');
 const eventsController = require('../controllers/events.controller');
 const authMiddleware = require('../middlewares/auth.middleware');
 const router = express.Router();
 
 module.exports = router;
 
-router.get('/', eventsController.index)
-
-
+router.get('/', (_, res) => {
+  res.render('events/index')
+})
 
 router.get('/users/new', usersController.new)
 router.post('/users', uploadCloud.array('images', 6), usersController.create)
 
-router.get('/login', usersController.login)
-router.post('/login', usersController.doLogin)
+router.get('/companies/new', companiesController.new)
+router.post('/companies', uploadCloud.single('logo'), companiesController.create)
 
+router.get('/login', (_, res) => {
+  res.render('login/index')
+});
+router.get('/login/users', usersController.login)
+router.post('/login/users', usersController.doLogin)
+router.get('/login/companies', companiesController.login)
+router.post('/login/companies', companiesController.doLogin)
