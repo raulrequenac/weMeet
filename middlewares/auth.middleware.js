437fc4d0ae0fa79df2ecaea0643c916004e4458b
raulrequenac <raulrequec@hotmail.com>
2019-12-19 21:42:26 +0100

    enroll & order by date

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -14,12 +14,3 @@ module.exports.isNotAuthenticated = (req, res, next) => {
     next();
   }
 }
-
-module.exports.canEditEvents = (req, res, next) => {
-  if (req.session.user.role === 'company') {
-    next();
-  } else {
-    req.session.genericError = 'You are not allowed to edit events!'
-    res.redirect('/');
-  }
-}
